ae7240a89da97d68a8bcebecf565b17f7d53d5fc
JanB11 <58641291+JanB11@users.noreply.github.com>
2023-02-20 10:53:35 +0000

    pot value global variable

--- client.py
+++ client.py
@@ -3,8 +3,10 @@ import socketio
 import struct
 from time import sleep
 
-
 sio = socketio.Client()
+sio.connect('http://192.168.0.31:5000')
+
+pot0_val = 0
 
 @sio.event
 def connect():
@@ -13,9 +15,15 @@ def connect():
 
 @sio.on('pot0_value')
 def response(pot0):
-    print('pot0: ', pot0)
-    
-    
+    global pot0_val
+    pot0_val = pot0
+
+def use_pot0_val():
+    global pot0_val
+    data = pot0_val
+    return data
+
+
 @sio.on('pot1_value')
 def response(pot1):
     print('pot1: ', pot1)
@@ -23,9 +31,15 @@ def response(pot1):
 @sio.event
 def disconnect():
     print('disconnected from server')
-
-sio.connect('http://172.20.10.10:5000')
-
     
+
+
+while True:
+    val = use_pot0_val()
+    print('Using pot0 value: ', val)
+    
+    
+#Hotspot IP: 172.20.10.10
+#Home IP: 192.168.0.31
     
     
